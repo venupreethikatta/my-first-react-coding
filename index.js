b5a3a18af47ad35592dbe8beb27b1726d07b8069
venupreethikatta <kattavenupreethi@gmail.com>
2023-06-19 11:38:22 +0000

    my first react coding

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+  //   Write your code here.
+  <div className="div-container">
+    <h1 className="heading-1">Super Over League</h1>
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+    <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
